e4670346af731b2421818b3efd2f646506065643
gucamole-hunter <hantothelei@protonmail.com>
2023-09-12 03:48:05 -0700

    port typo fixed =.=

--- pdf_processor.py
+++ pdf_processor.py
@@ -12,9 +12,9 @@ import joblib
 # Connect to Redis
 try:
     # for local use
-    redis_db = redis.StrictRedis(host='localhost', port=6379, db=0)
+    #redis_db = redis.StrictRedis(host='localhost', port=6379, db=0)
     # when using with docker
-    # redis_db = redis.StrictRedis(host='redis', port=6369, db=0)
+    redis_db = redis.StrictRedis(host='redis', port=6379, db=0)
     print("Successfully connected to Redis.")
 except redis.ConnectionError:
     print("Error: Unable to connect to Redis.")
